7b8470369c572b1d3f1da2cc39992e0363c9a3da
Asiya <asiya_ermolaeva@mail.ru>
2023-04-23 16:01:34 +0300

    add modal window

--- src/App.js
+++ src/App.js
@@ -1,23 +1,25 @@
-import logo from './logo.svg';
-import './App.css';
+import {useState} from 'react'
+import './index.scss';
 
 function App() {
+const [open, setOpen] = useState(false);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <button onClick={()=> setOpen(true)} className="open-modal-button"> Открыть окно </button>
+      {
+        open && (
+          <div className="overlay">
+            <div className="modal">
+              <svg onClick={()=> setOpen(false)} height="200" viewBox="0 0 200 200" width="200">
+                <path d="M114,100l49-49a9.9,9.9,0,0,0-14-14L100,86,51,37A9.9,9.9,0,0,0,37,51l49,49L37,149a9.9,9.9,0,0,0,14,14l49-49,49,49a9.9,9.9,0,0,0,14-14Z" />
+              </svg>
+              <img width ="300px"src="https://media1.giphy.com/media/l2Jho5fnv7sfNAAZq/giphy.gif?cid=ecf05e47y4bezle9o7luw7q5f3z3zusxt7510c1ohj1bqt5k&rid=giphy.gif" alt='unicorn'/>
+            </div>
+          </div>
+          )
+      }
+      {/*  */}
     </div>
   );
 }
