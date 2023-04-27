3886c26d811239748421ba8d6ea5a6e4c698c27c
Asiya <asiya_ermolaeva@mail.ru>
2023-04-27 23:55:25 +0300

    add currency-convertor component

--- src/App.js
+++ src/App.js
@@ -1,12 +1,67 @@
-import React from 'react'
-
-import './index.scss'
-
+import React, { useState, useEffect, useRef } from 'react';
+import { Block } from './Block';
+import './index.scss';
 
 function App() {
+  const [fromCurrency, setFromCurrency] = useState('RUB');
+  const [toCurrency, setToCurrency] = useState('USD');
+  const [fromPrice, setFromPrice] = useState(0);
+  const [toPrice, setToPrice] = useState(1);
+
+  // const [rates, setRates] = useState({}); //асинхронная
+  const ratesRef = useRef({});
+
+  useEffect(() => {
+    fetch('https://www.cbr-xml-daily.ru/latest.js')
+      .then((res) => res.json())
+      .then((json) => {
+        const fullRates = json.rates;
+        fullRates[`${json.base}`]= 1;
+        ratesRef.current = fullRates;
+        onChangeToPrice(1);
+      })
+      .catch((err) => {
+        console.warn(err);
+        alert('Не удалось получить информацию');
+      });
+  }, []);
+
+  const onChangeFromPrice = (value) => {
+    const price = value / ratesRef.current[fromCurrency];
+    const result = price * ratesRef.current[toCurrency];
+    setFromPrice(value);
+    setToPrice(result.toFixed(3));
+
+  };
+
+  const onChangeToPrice = (value) => {
+    const result = value * ratesRef.current[fromCurrency] / ratesRef.current[toCurrency];
+    setToPrice(value);
+    setFromPrice(result.toFixed(3));
+  };
+
+  useEffect(() => {
+    onChangeFromPrice(fromPrice);
+  }, [fromCurrency]);
+
+  useEffect(() => {
+    onChangeToPrice(toPrice);
+  }, [toCurrency]);
 
   return (
     <div className="App">
+      <Block
+        value={fromPrice}
+        currency={fromCurrency}
+        onChangeCurrency={setFromCurrency}
+        onChangeValue={onChangeFromPrice}
+      />
+      <Block
+        value={toPrice}
+        currency={toCurrency}
+        onChangeCurrency={setToCurrency}
+        onChangeValue={onChangeToPrice}
+      />
     </div>
   );
 }
